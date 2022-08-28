f005185a38abf390908bb5a57941aaedc126b005
davidwagih <78867146+david-wagih@users.noreply.github.com>
2022-08-28 11:38:22 +0200

    some changes

--- components/DropZone.tsx
+++ components/DropZone.tsx
@@ -1,47 +1,39 @@
 import React, { useEffect, useState } from "react";
 import { Dropzone, FileItem } from "@dropzone-ui/react";
-import { uploadFile } from "../services/googleDrive";
 import { getData, getId, getTranscript } from "../services/speechAnalytics";
-import useDrivePicker from "react-google-drive-picker/dist";
-import { config } from "../config/config";
 
 function DropZone() {
-  const [openPicker, authResponse] = useDrivePicker();
   const [files, setFiles] = useState([]);
   const updateFiles = (incommingFiles: any) => {
     setFiles(incommingFiles);
   };
 
-  console.log(files[0]);
-
-  // todo : try to test uploading and event getting the transcript for a start
   const handleUpload = (file: any) => {
-    // uploadFile(file);
     getId(file);
     console.log("hello");
     console.log(file);
     console.log(getData(file));
   };
 
-  const handleOpenPicker = () => {
-    openPicker({
-      clientId: config.clientID,
-      developerKey: config.apiKey,
-      viewId: "DOCS",
-      // token: token, // pass oauth token in case you already have one
-      token: config.accessToken,
-      showUploadView: true,
-      showUploadFolders: true,
-      supportDrives: true,
-      // customViews: customViewsArray, // custom view
-      callbackFunction: (data) => {
-        if (data.action === "cancel") {
-          console.log("User clicked cancel/close button");
-        }
-        console.log(data);
-      },
-    });
-  };
+  // const handleOpenPicker = () => {
+  //   openPicker({
+  //     clientId: config.clientID,
+  //     developerKey: config.apiKey,
+  //     viewId: "DOCS",
+  //     // token: token, // pass oauth token in case you already have one
+  //     token: config.accessToken,
+  //     showUploadView: true,
+  //     showUploadFolders: true,
+  //     supportDrives: true,
+  //     // customViews: customViewsArray, // custom view
+  //     callbackFunction: (data) => {
+  //       if (data.action === "cancel") {
+  //         console.log("User clicked cancel/close button");
+  //       }
+  //       console.log(data);
+  //     },
+  //   });
+  // };
 
   return (
     <div>
@@ -60,9 +52,7 @@ function DropZone() {
           <FileItem key={file.id} {...file} preview />
         ))}
       </Dropzone>
-      <div>
-        <button onClick={() => handleUpload(files[0])}>upload</button>
-      </div>
+      <button onClick={() => handleUpload(files[0])}>upload</button>
     </div>
   );
 }
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,11 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+// @ts-ignore
 import { ChakraProvider } from "@chakra-ui/react";
 import theme from "../styles/theme";
 
-function MyApp({ Component, pageProps }: AppProps) {
+// @ts-ignore
+function MyApp({ Component, pageProps }) {
   return (
     <ChakraProvider theme={theme}>
       <Component {...pageProps} />
